1f67ae70196635b6756df9489f20b0df40f4bb29
vaifav <vaifav06ai@gmail.com>
2026-04-27 10:24:42 +0000

    Completed the website review part

--- ui/src/components/home/Home.tsx
+++ ui/src/components/home/Home.tsx
@@ -3,6 +3,7 @@ import Categories from "../categories/home/Categories";
 import Connect from "../connect/Connect";
 import Hero from "../hero/Hero";
 import OurProcess from "../our_process/OurProcess";
+import Review from "../reviews/home/Review";
 import CommonSection from "./CommonSection";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
 			<CommonSection Component={Categories} text="Find your <br/> perfect" emphasizedText="flavour" dividerText="Browse Categories"/>
 			<CommonSection Component={BestSellers} text="Bestsellers" dividerText="Most loved" linkText="View all" className="bg-secondary **:text-white"/>
 			<CommonSection Component={OurProcess} text="From our kitchen <br/> to your" emphasizedText="table" dividerText="simple process" hasLink={false}/>
+			<CommonSection Component={Review} text="Real Families,<br/> Real" emphasizedText="flavour" dividerText="what people say" hasLink={false}/>
 			<Connect />
 		</>
 	);
---
+++ ui/src/components/reviews/home/Review.tsx
@@ -0,0 +1,31 @@
+import { Star } from "lucide-react";
+import { DUMMY_REVIEWS } from "../../../utils/constants";
+
+const ICON_NUMBER = [...Array(5)];
+const Review = () => {
+	return (
+		<section className="grid gap-5 md:grid-cols-2 lg:grid-cols-4">
+			{DUMMY_REVIEWS.map(({ name, review, place }) => {
+				return (
+					<div className="flex flex-col justify-between shadow rounded-xl p-4 text-secondary">
+						<div className="flex items-center gap-1">
+							{ICON_NUMBER.map((_) => {
+								return <Star className="fill-rust-200 size-3 stroke-0" />;
+							})}
+						</div>
+                        <p className="italic py-3 font-normal leading-[1.75] text-[15px]">&ldquo;{review}&rdquo;</p>
+                        <div className="flex items-start gap-3">
+                            <span className="grid place-items-center size-10 bg-secondary-text/10 rounded-full border-2 border-secondary-text/40 uppercase font-medium text-[12px]"><span>{name[0]+name[1]}</span></span>
+                            <div>
+                                <h3 className="font-semibold font-serif text-[14px]">{name}</h3>
+                                <p className="text-secondary-text italic text-[12px]">{place}</p>
+                            </div>
+                        </div>
+					</div>
+				);
+			})}
+		</section>
+	);
+};
+
+export default Review;
